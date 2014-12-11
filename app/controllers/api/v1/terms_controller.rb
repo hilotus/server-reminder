953a873a253b2c6df336c9b62fa5095515687d47
CBluowei <wei.luo@careerbuilder.com>
2014-12-12 03:49:19 +0800

    term find

--- app/controllers/api/v1/terms_controller.rb
+++ app/controllers/api/v1/terms_controller.rb
@@ -2,11 +2,7 @@ module Api
   module V1
     class TermsController < ApplicationController
       def find
-        if params['where'].nil?
-          terms = Term.by_creator(params['creator'])
-        else
-          terms = Term.by_hash(params['where'])
-        end
+        terms = Term.by_hash(params['where'])
         respond_with_jsons terms
       end
     end
