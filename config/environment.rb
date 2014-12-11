aae7ad808013e491e7e2af11ada5c31812867512
rFang0414 <Riu.Fang@careerbuilder.com>
2014-12-11 22:17:21 +0800

    lunar to solar

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+classpath = ["/lib/lunarToSolar.jar"]
 # Initialize the rails application
 ServerReminder::Application.initialize!
