21df8fb5dbaeb871dd1cbdab0272261cdf0e1e38
CBluowei <wei.luo@careerbuilder.com>
2014-12-12 11:21:58 +0800

    use GMT Time to compare

--- app/jobs/reminder.rb
+++ app/jobs/reminder.rb
@@ -4,20 +4,13 @@ require 'rake'
 
 class Reminder
   def self.perform
-    Rails.logger.debug("111111111111111111111111")
-    Rails.logger.debug("111111111111111111111111")
-    Rails.logger.debug("111111111111111111111111")
-
     Event.each do |event|
       lunar_time = event.datetime.split.last
       solar_date = $lunartosolar.lunarTosolar(event.datetime.split.first)
 
-      now = Time.now.to_s.split
+      now = Time.now.gmtime.to_s.split
 
       if solar_date.eql?(now.first) and lunar_time.eql?(now[1].split(":").first(2).join(":"))
-        Rails.logger.debug("222222222222222222222222")
-        Rails.logger.debug("222222222222222222222222")
-        Rails.logger.debug("222222222222222222222222")
         user = User.by_user_id(event.user).first
         channelid = user.token.split('_').first
         userid = user.token.split('_').last
@@ -28,10 +21,6 @@ class Reminder
         messages = {title: '', description: event.title, notification_basic_style: 7, open_type: 2}
         params = {message_type: 1, device_type: 4, user_id: userid, channel_id: channelid}
         client.push_msg(1, messages, Time.now.to_s, params)
-
-        Rails.logger.debug("33333333333333333333333")
-        Rails.logger.debug("33333333333333333333333")
-        Rails.logger.debug("33333333333333333333333")
       end
     end
   end
